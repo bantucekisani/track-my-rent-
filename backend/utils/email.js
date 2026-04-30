a3c2d6d464c97b68940ad1fa2b934d1ad16d7f15
Bantu Cekisani <bantucekisani@gmail.com>
2026-05-01 01:00:02 +0200

    Standardize remaining PDF and email helpers

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,19 +1,5 @@
-// utils/email.js
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  service: "gmail", 
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
-  },
-});
+const sendEmail = require("./email/sendEmail");
 
 exports.sendEmail = async (to, subject, html) => {
-  return transporter.sendMail({
-    from: `"Track My Rent" <${process.env.MAIL_USER}>`,
-    to,
-    subject,
-    html,
-  });
+  return sendEmail({ to, subject, html });
 };
--- backend/utils/pdf/generateTenantStatementHTML.js
+++ backend/utils/pdf/generateTenantStatementHTML.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const ejs = require("ejs");
-const puppeteer = require("puppeteer");
+const renderHTMLToPDF = require("./renderHTMLToPDF");
 
 module.exports = async function generateTenantStatementHTML(data) {
   const templatePath = path.join(
@@ -10,30 +10,5 @@ module.exports = async function generateTenantStatementHTML(data) {
   );
 
   const html = await ejs.renderFile(templatePath, data);
-
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"]
-  });
-
-  const page = await browser.newPage();
-
-  await page.setContent(html, {
-    waitUntil: "networkidle0"
-  });
-
-  const pdf = await page.pdf({
-    format: "A4",
-    printBackground: true,
-    margin: {
-      top: "20mm",
-      bottom: "20mm",
-      left: "15mm",
-      right: "15mm"
-    }
-  });
-
-  await browser.close();
-
-  return pdf;
+  return renderHTMLToPDF(html);
 };
